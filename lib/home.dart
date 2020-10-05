9f6c0cbad910e6bc0cacab42bf855b82b7a64236
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2020-10-05 19:07:44 -0300

    04_Criando_Speed_Dial

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:flutter_speed_dial_app/pages.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -8,6 +10,57 @@ class HomeScreen extends StatelessWidget {
         title: Text("Flutter SpeedDial"),
         centerTitle: true,
       ),
+      floatingActionButton: buildSpeedDial(context),
+    );
+  }
+
+  SpeedDial buildSpeedDial(BuildContext context) {
+    return SpeedDial(
+      animatedIcon: AnimatedIcons.menu_close,
+      children: [
+        SpeedDialChild(
+          child: Icon(Icons.contacts, color: Colors.white,),
+          backgroundColor: Colors.amber,
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => PagesScreen("Contatos"),
+              )
+            );
+          },
+          label: "Contatos",
+          labelStyle: TextStyle(color: Colors.white),
+          labelBackgroundColor: Colors.amber
+        ),
+        SpeedDialChild(
+            child: Icon(Icons.history, color: Colors.white,),
+            backgroundColor: Colors.red,
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => PagesScreen("Histórico"),
+                  )
+              );
+            },
+            label: "Histórico",
+            labelStyle: TextStyle(color: Colors.white),
+            labelBackgroundColor: Colors.red
+        ),
+        SpeedDialChild(
+            child: Icon(Icons.favorite, color: Colors.white,),
+            backgroundColor: Colors.blueGrey,
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => PagesScreen("Favoritos"),
+                  )
+              );
+            },
+            label: "Favoritos",
+            labelStyle: TextStyle(color: Colors.white),
+            labelBackgroundColor: Colors.blueGrey
+        ),
+      ],
     );
   }
 }
